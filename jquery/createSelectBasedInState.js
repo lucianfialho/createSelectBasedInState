2c1f816a454fbb1e47457781620a9c3639dff638
Lucian Fialho <lucianfialhobp@bitbucket.org>
2014-06-09 09:23:19 -0300

    callback para desenvolvimento local e manipulando retorno dos dados

--- jquery/createSelectBasedInState.js
+++ jquery/createSelectBasedInState.js
@@ -2,19 +2,15 @@
     $.fn.EstadosToCidades = function(options) {
         return this.change(function() {
           var valorEstado = this.value;
+          var returnData;  
             $.ajax({
                 url: options.pathOfJson,
                 dataType: "json",
                 type: "get",
             }).done(function(response){
-                $.map(response, function(el) {
-                    if(el.sigla === "A"){
-                        console.log(el)
-                    }
-                });
-                console.log(response);
+                returnData = response;
+                console.log(returnData.estados);
             });
-
         });
     }
 })(jQuery);
