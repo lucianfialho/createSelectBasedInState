9d259c23f10eac463a2c2806048c8c15abfa3756
Lucian Fialho <lucianfialhobp@bitbucket.org>
2014-06-09 11:49:41 -0300

    motando loop que procura os itens no array e adicionando o retorno no output

--- jquery/createSelectBasedInState.js
+++ jquery/createSelectBasedInState.js
@@ -2,14 +2,22 @@
     $.fn.EstadosToCidades = function(options) {
         return this.change(function() {
           var valorEstado = this.value;
-          var returnData;  
+          var returnData = [];
             $.ajax({
                 url: options.pathOfJson,
                 dataType: "json",
                 type: "get",
             }).done(function(response){
-                returnData = response;
-                console.log(returnData.estados);
+                $.each(response.estados, function(i, v){
+                    if(v.sigla == valorEstado){
+                        $.each(v.cidades, function(index,val){
+                            returnData += '<option value='+ val +'>' + val +'</option>';
+                        });
+                    }else {
+                        console.log("Não encontrei cidades baseadas na sua UF");
+                    }
+                });
+                $(options.selectToOutput).html(returnData);
             });
         });
     }
