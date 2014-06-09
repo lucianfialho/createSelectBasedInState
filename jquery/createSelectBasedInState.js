9925821fa2c2f0946e8ca409f2e52c25c2b69689
Lucian Fialho <lucianfialhobp@bitbucket.org>
2014-06-09 13:30:13 -0300

    separando requisicao do each para ter uma requisicao so

--- jquery/createSelectBasedInState.js
+++ jquery/createSelectBasedInState.js
@@ -1,24 +1,27 @@
 (function($){
     $.fn.EstadosToCidades = function(options) {
-        return this.change(function() {
-          var valorEstado = this.value;
-          var returnData = [];
-            $.ajax({
-                url: options.pathOfJson,
-                dataType: "json",
-                type: "get",
-            }).done(function(response){
-                $.each(response.estados, function(i, v){
-                    if(v.sigla == valorEstado){
-                        $.each(v.cidades, function(index,val){
-                            returnData += '<option value='+ val +'>' + val +'</option>';
-                        });
-                    }else {
-                        console.log("Não encontrei cidades baseadas na sua UF");
-                    }
-                });
-                $(options.selectToOutput).html(returnData);
-            });
+        var jsonReturn;
+        $.ajax({
+            url: options.pathOfJson,
+            dataType: "json",
+            type: "get",
+        }).done(function(response){
+            jsonReturn = response;
         });
+        
+        return this.change(function() {
+            var valorEstado = this.value;
+            var returnData = [];
+
+            $.each(jsonReturn.estados, function(i, v){
+                if(v.sigla === valorEstado){
+                    $.each(v.cidades, function(index,val){
+                        returnData += '<option value='+ val +'>' + val +'</option>';
+                    });
+                }
+            });
+            $(options.selectToOutput).html(returnData);
+        });
+        
     }
 })(jQuery);
